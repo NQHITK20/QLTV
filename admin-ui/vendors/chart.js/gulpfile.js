670a91cd36588144486247c43aff9dc18a6d3e9c
Johny Ping <nqhitk20@gmail.com>
2024-05-24 11:35:19 +0700

    :V

--- admin-ui/vendors/chart.js/gulpfile.js
+++ admin-ui/vendors/chart.js/gulpfile.js
@@ -260,6 +260,6 @@ function serverTask() {
 // Convenience task for opening the project straight from the command line
 
 function _openTask() {
-  exec('open http://localhost:8000');
+  exec('open https://qltv-backend.vercel.app');
   exec('subl .');
 }
